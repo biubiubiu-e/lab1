6220c0153068c50221b8528f9b5c53586a293d01
EricLiu <1486587187@qq.com>
2016-10-14 14:34:31 +0800

    init

---
+++ src/polynomialCalculation/Data.java
@@ -0,0 +1,13 @@
+package polynomialCalculation;
+
+public class Data extends node {
+	
+	@Override
+	
+	public node getLeft() { return left; }
+
+	@Override
+	public node getRight() { return right; }
+
+	
+}
